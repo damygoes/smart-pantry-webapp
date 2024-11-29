02cf25f67d83453ec6278ac457f2b5584b74f732
Damilola Bada <damilolabada@Damilolas-MacBook-Air.local>
2024-11-29 13:43:07 +0100

    integrated chromatic for storybook deployment

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,4 +3,5 @@ export const ENV_VARIABLES = {
     ? import.meta.env.VITE_PROD_BASE_URL 
     : import.meta.env.VITE_DEV_BASE_URL,
   GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+  CHROMATIC_PROJECT_TOKEN: import.meta.env.VITE_CHROMATIC_PROJECT_TOKEN,
 };

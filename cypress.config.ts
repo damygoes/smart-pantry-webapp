c2051f1cb07ce21e7121e32b277d3399877ba70c
Damilola Bada <damilolabada@Damilolas-MacBook-Air.local>
2024-11-29 11:48:46 +0100

    modified pipeline by setting env to production before running e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,12 +8,18 @@ export default defineConfig({
   e2e: {
     video: true,
     screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
     baseUrl: process.env.VITE_ENV === 'production'
       ? process.env.VITE_PROD_BASE_URL
       : process.env.VITE_DEV_BASE_URL,
+    retries: {
+      runMode: 2,  // Retry up to 2 times when running in CI
+      openMode: 0, // No retries when running interactively
+    },
     setupNodeEvents(on, config) {
-      // You can set up other events here if needed
+      console.log('Cypress baseUrl:', config.baseUrl);
       return config;
     },
-  },
+  }
+  
 });

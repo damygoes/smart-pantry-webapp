ade3b9e3a931fecf93320663b284ec803f14603f
Damilola Bada <damilolabada@Damilolas-MacBook-Air.local>
2024-11-29 11:32:21 +0100

    modified pipeline to delay e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,8 @@ dotenv.config();
 
 export default defineConfig({
   e2e: {
+    video: true,
+    screenshotsFolder: 'cypress/screenshots',
     baseUrl: process.env.VITE_ENV === 'production'
       ? process.env.VITE_PROD_BASE_URL
       : process.env.VITE_DEV_BASE_URL,
--- cypress/e2e/1-homepage/homepage.cy.ts
+++ cypress/e2e/1-homepage/homepage.cy.ts
@@ -3,11 +3,12 @@
 describe('Home Page', () => {
   beforeEach(() => {
     // Visit the home page before each test
-    cy.visit('/') 
+    cy.visit('/')
+    cy.url().should('include', 'smart-pantry-webapp.vercel.app');
   })
 
   it('should display the home page content', () => {
     // Check if the title is visible
-    cy.get('h1').should('be.visible').and('contain.text', 'Welcome to Smart Pantry')
+    cy.get('h1', {timeout: 10000}).should('be.visible').and('contain.text', 'Welcome to Smart Pantry')
   })
 })

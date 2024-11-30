916787514e1e6f49459312d5f6fcfc5215acdb16
Damilola Bada <damilolabada@Damilolas-MacBook-Air.local>
2024-11-30 21:12:27 +0100

    modified biome.json and eslint config

--- src/components/ui/button/Button.stories.tsx
+++ src/components/ui/button/Button.stories.tsx
@@ -4,59 +4,59 @@ import { fn } from '@storybook/test';
 import { Button } from './Button';
 
 const meta = {
-  title: 'Primitives/Button',
-  component: Button,
-  tags: ['autodocs'],
-  argTypes: {
-    variant: {
-      description: 'The variant of the button',
-      options: [
-        'primary',
-        'destructive',
-        'outline',
-        'secondary',
-        'ghost',
-        'link',
-      ],
-      control: { type: 'radio' },
-      table: {
-        defaultValue: { summary: 'primary' },
-        type: { summary: 'string' },
-      },
-    },
-    size: {
-      description: 'How large should the button be?',
-      options: ['sm', 'md', 'lg'],
-      control: { type: 'radio' },
-      table: {
-        defaultValue: { summary: 'md' },
-        type: { summary: 'string' },
-      },
-    },
-    radius: {
-      description: 'The radius of the button',
-      options: ['none', 'md', 'lg', 'full'],
-      control: { type: 'radio' },
-      table: {
-        defaultValue: { summary: 'md' },
-        type: { summary: 'string' },
-      },
-    },
-    asChild: {
-      description: 'If true, the button will be rendered as a child of a Slot',
-      control: { type: 'boolean' },
-    },
-    onClick: {
-      description: 'Optional click handler',
-      action: 'clicked',
-    },
-  },
-  args: {
-    variant: 'primary',
-    size: 'md',
-    onClick: fn(),
-    children: 'Default Button Text',
-  },
+	title: 'Primitives/Button',
+	component: Button,
+	tags: ['autodocs'],
+	argTypes: {
+		variant: {
+			description: 'The variant of the button',
+			options: [
+				'primary',
+				'destructive',
+				'outline',
+				'secondary',
+				'ghost',
+				'link',
+			],
+			control: { type: 'radio' },
+			table: {
+				defaultValue: { summary: 'primary' },
+				type: { summary: 'string' },
+			},
+		},
+		size: {
+			description: 'How large should the button be?',
+			options: ['sm', 'md', 'lg'],
+			control: { type: 'radio' },
+			table: {
+				defaultValue: { summary: 'md' },
+				type: { summary: 'string' },
+			},
+		},
+		radius: {
+			description: 'The radius of the button',
+			options: ['none', 'md', 'lg', 'full'],
+			control: { type: 'radio' },
+			table: {
+				defaultValue: { summary: 'md' },
+				type: { summary: 'string' },
+			},
+		},
+		asChild: {
+			description: 'If true, the button will be rendered as a child of a Slot',
+			control: { type: 'boolean' },
+		},
+		onClick: {
+			description: 'Optional click handler',
+			action: 'clicked',
+		},
+	},
+	args: {
+		variant: 'primary',
+		size: 'md',
+		onClick: fn(),
+		children: 'Default Button Text',
+	},
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -64,43 +64,43 @@ type Story = StoryObj<typeof meta>;
 
 // Button template that takes in args dynamically
 const Template = (args: Record<string, unknown>) => (
-  <Button {...args}> Add meal entry</Button>
+	<Button {...args}> Add meal entry</Button>
 );
 const LoadingTemplate = (args: Record<string, unknown>) => (
-  <Button {...args}>Processing...</Button>
+	<Button {...args}>Processing...</Button>
 );
 
 export const Default: Story = {
-  render: Template,
-  args: {
-    variant: 'primary',
-    size: 'md',
-  },
+	render: Template,
+	args: {
+		variant: 'primary',
+		size: 'md',
+	},
 };
 
 export const WithPrefixIcon: Story = {
-  render: Template,
-  args: {
-    variant: 'primary',
-    size: 'md',
-    iconBefore: <PlusIcon />,
-  },
+	render: Template,
+	args: {
+		variant: 'primary',
+		size: 'md',
+		iconBefore: <PlusIcon />,
+	},
 };
 
 export const WithSuffixIcon: Story = {
-  render: Template,
-  args: {
-    variant: 'primary',
-    size: 'md',
-    iconAfter: <PlusIcon />,
-  },
+	render: Template,
+	args: {
+		variant: 'primary',
+		size: 'md',
+		iconAfter: <PlusIcon />,
+	},
 };
 
 export const WithLoader: Story = {
-  render: LoadingTemplate,
-  args: {
-    variant: 'primary',
-    size: 'md',
-    isLoading: true,
-  },
+	render: LoadingTemplate,
+	args: {
+		variant: 'primary',
+		size: 'md',
+		isLoading: true,
+	},
 };
--- src/utils/createIcon.tsx
+++ src/utils/createIcon.tsx
@@ -2,16 +2,16 @@ import type { ComponentPropsWithoutRef, ElementType } from 'react';
 
 // Extend SVGProps to include `size`
 type IconProps = ComponentPropsWithoutRef<'svg'> & {
-  size?: number;
+	size?: number;
 };
 
 // Helper function to create icons with common properties
 export const createIcon = (IconComponent: ElementType) => {
-  if (!IconComponent) {
-    throw new Error('IconComponent is required');
-  }
+	if (!IconComponent) {
+		throw new Error('IconComponent is required');
+	}
 
-  return ({ size = 15, ...props }: IconProps) => (
-    <IconComponent width={size} height={size} {...props} />
-  );
+	return ({ size = 15, ...props }: IconProps) => (
+		<IconComponent width={size} height={size} {...props} />
+	);
 };
